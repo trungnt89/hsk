c17cb0ed392b21ef41c9568227f0b46e218bf3c4
trungnt89 <46738728+trungnt89@users.noreply.github.com>
2025-12-29 00:42:08 +0900

    Add files via upload
    Signed-off-by: trungnt89 <46738728+trungnt89@users.noreply.github.com>

--- pmp/data1.js
+++ pmp/data1.js
@@ -1,5 +1,5 @@
 // File data1.js
-const DATA_SET_1 = [
+window.DATA_SET_1 = [
   {
     "id": 1,
     "question": "The night before a standup meeting, a project manager realizes that they will be unable to attend the daily standup meeting in person but will join via phone. The project manager sent an email to the project team letting them know. One of the team members is upset that, despite having the longest commute, they are expected to attend the meeting in person.\nHow should the project manager address this situation?",

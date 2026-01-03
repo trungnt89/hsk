23e380b837efc7f2cf0fb9846ebde01bb810cb4e
test@test.com <test@test.com>
2026-01-04 01:04:08 +0900

    SRC change

--- todo/sw.js
+++ todo/sw.js
@@ -1,20 +1,18 @@
 // sw.js
-self.addEventListener('push', function(event) {
-    const options = {
-        body: 'Đây là tin nhắn từ Service Worker!',
-        icon: 'https://cdn-icons-png.flaticon.com/512/252/252035.png'
-    };
-
-    event.waitUntil(
-        self.registration.showNotification('Hello World!', options)
-    );
+self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Ép SW mới hoạt động ngay
 });
 
-// Hàm tự kích hoạt thông báo để test ngay khi cài đặt xong
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim()); // Kiểm soát tất cả các tab ngay lập tức
+});
+
 self.onmessage = (event) => {
+    console.log('SW nhận được lệnh:', event.data);
     if (event.data === 'trigger-notify') {
         self.registration.showNotification('Hello World!', {
             body: 'Thông báo đã được kích hoạt thành công!',
+            icon: 'https://cdn-icons-png.flaticon.com/512/252/252035.png'
         });
     }
 };

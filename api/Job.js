fce090b919e2207146921fd61993540e8576101a
test@test.com <test@test.com>
2026-05-17 10:36:08 +0900

    SRC change

--- api/Job.js
+++ api/Job.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
 
         res.status(200).json({ status: "Success", timezone: "JST", sentIds });
     } catch (error) {
-        writeLog("[ERROR]", error);
+        writeLog("[ERROR]", error.message);
         res.status(500).json({ error: error.message });
     }
 }

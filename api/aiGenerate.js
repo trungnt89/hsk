b27621ecec3d51511d9dba5835eb9ec546b71cb6
test@test.com <test@test.com>
2026-05-24 00:13:37 +0900

    SRC change

--- api/aiGenerate.js
+++ api/aiGenerate.js
@@ -27,6 +27,8 @@ export default async function handler(req, res) {
   ///////////////////////////////////////////////////
     await util.ensureAuthenticated();
   let resPromt = await util.handleReadByPosVal(spreadsheetId, SHEET_2, 2, 1);
+  const json1 = await resPromt.json();
+  writeLog(JSON.stringify(json1));
   promt = (resPromt.values[0])[1];
   ///////////////////////////////////////////////////
   ///////////////////////////////////////////////////

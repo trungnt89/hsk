996fead2d2dc848eb28d2fb312b0133f9b7a2107
test@test.com <test@test.com>
2026-06-14 00:06:43 +0900

    SRC change

--- api/gSheet.js
+++ api/gSheet.js
@@ -7,6 +7,14 @@ import * as util from './com/sheet';
 
 
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     const { method, query, body } = req;
 
     const p = { ...query, ...body };

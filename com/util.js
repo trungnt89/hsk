57766948c8734b7584b61e25af4c116b287a9b7d
test@test.com <test@test.com>
2026-06-21 22:43:21 +0900

    SRC change

--- com/util.js
+++ com/util.js
@@ -71,6 +71,7 @@ async function callAjax(url, body) {
         const response = await fetch(url, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
+			credentials: "include", 
             body: JSON.stringify(body)
         });
         return await response.json();
